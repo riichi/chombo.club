5a650856202786a4192d499d5cc95e6dacc44093
m4tx <m4tx@users.noreply.github.com>
2021-06-16 13:59:34 +0000

    jekyll build from Action d76145fe70124a69ad3e814deb26a1440d8624c3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Wesołych Świąt!",
+        "excerpt":"Krakowski Klub Chombo życzy wszystkim Wesołych Świąt i Szczęśliwego Nowego Roku! Niech 2020 przyniesie Wam wiele Kokushi Musou, Daisangenów i Suuankou!  ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2019/12/24/merry-christmas/",
+        "teaser": null
+      },{
+        "title": "Michał Herda sędzią USMA",
+        "excerpt":"Z radością informujemy, że nasz klubowy kolega Michał Herda został certyfikowanym sędzią Górnośląskiego Związku Madżonga. Będzie on mógł od teraz sędziować gry rankingowe USMA oraz nie-MERS-owe (USMA) turnieje. Gratulacje!          ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/01/20/new-referee-michal-herda/",
+        "teaser": null
+      },{
+        "title": "Krakowski Klub Chombo na Mahjong Retro Taikai",
+        "excerpt":"Czteroosobowa reprezentacja Krakowskiego Klubu Chombo wybrała się na Retro Mahjong Taikai i przywiozła trofea w postaci zupełnie niczego! Turniej był wspaniały, graliśmy na bambusowych kamieniach, a Mateusz Maćkowski dorobił się w warszawskim Auchanie nowej emotikony.          ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/02/23/mahjong-retro-taikai/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — pierwszy odcinek już na YouTube!",
+        "excerpt":"Na polskim YouTube brakuje wartościowych materiałów o mahjongu dla początkujących. Początkujący chcący zagrać w tę chińską grę towarzyską skazani są na wideo w języku angielskim lub na materiały w formie tekstowej po polsku o często wątpliwej jakości. Osoba kompletnie nieobeznana w temacie oraz bez kogoś, kto pomógłby jej nauczyć się...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/09/05/mahjong-basics-01/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — drugi odcinek już na YouTube!",
+        "excerpt":"Drugi odcinek naszej serii filmów o podstawach rīchi mahjonga znalazł się na kanale Krakowskiego Klubu Chombo na YouTube.. Pokazujemy na nim, w jaki sposób poprawnie przygotować grę, to znaczy: usadzić graczy, przygotować mur, rozdać punkty, oraz dobrać ręce startowe.   Kolejny, trzeci odcinek, już niebawem!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/10/03/mahjong-basics-02/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — trzeci odcinek już na YouTube!",
+        "excerpt":"Trzeci odcinek naszej serii filmów o podstawach rīchi mahjonga znalazł się na kanale Krakowskiego Klubu Chombo na YouTube.. Przechodzimy w nim wreszcie do podstaw przebiegu gry, czyli procesu budowania ręki przy użyciu kamieni z muru oraz deklarowania wygranej.   Czwarty odcinek już wkrótce!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/05/mahjong-basics-03/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — czwarty odcinek już na YouTube!",
+        "excerpt":"Czwarty odcinek naszej serii filmów o podstawach rīchi mahjonga znalazł się na kanale Krakowskiego Klubu Chombo na YouTube. Opowiadamy w nich o sztuce złodziejstwa, czyli meldowania kamieni wyrzuconych przez przeciwnika, oraz o zasadach dotyczących czwórek.   Kolejne odcinki już wkrótce!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/29/mahjong-basics-04/",
+        "teaser": null
+      },{
+        "title": "Ekwipunek: recenzja zestawu białego z e-go.pl",
+        "excerpt":"Cześć! Tutaj Michał Herda z Krakowskiego Klubu Chombo. Dzisiaj prezentuję recenzję zestawu do mahjonga japońskiego z internetowego sklepu e-go.pl. Zestaw w momencie zakupu kosztował 269 PLN i zawiera kamienie oraz akcesoria do gry w mahjonga japońskiego. W trakcie recenzji zestaw będę tu i ówdzie porównywał do (porządnie już zużytego) egzemplarza...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/06/15/review-e-go-white-tiles/",
+        "teaser": null
+      }]
---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Merry Christmas!",
+        "excerpt":"Kraków Chombo Club wishes you a Merry Christmas and Happy New Year! May the 2020 bring you many Kokushi Musous, Daisangens and Suuankous!  ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2019/12/24/merry-christmas/",
+        "teaser": null
+      },{
+        "title": "Michał Herda has become a USMA referee",
+        "excerpt":"We are happy to announce that our colleague Michał Herda has become an Upper Silesian Mahjong Association-certified referee. He is now eligible for refereeing USMA ranked games as well as non-MERS (USMA) tournaments. Congratulations!          ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/01/20/new-referee-michal-herda/",
+        "teaser": null
+      },{
+        "title": "Krakowski Klub Chombo on Mahjong Retro Taikai",
+        "excerpt":"Kraków Chombo Club participated in Retro Mahjong Taikai, proudly represented by four of our members - unfortunately, with no prizes this time :( The tournament was a great occasion to meet Mahjong freaks from other parts of Poland as well as try out playing with bamboo tiles. Plus Mateusz Maćkowski...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/02/23/mahjong-retro-taikai/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the first episode is live on YouTube!",
+        "excerpt":"YouTube lacks high-quality tutorials about Mahjong for beginners, especially in Polish. Newcomers that want to play this chinese game are therefore forced to watch videos in English or read text guides in Polish that are often not very great. A person who is completely new to the game that does...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/09/05/mahjong-basics-01/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the second episode is live on YouTube!",
+        "excerpt":"The second episode of our series describing the rīchi mahjong basics landed on the YouTube channel of Kraków Chombo Club.. We show how to properly prepare the game, that is: seat the players, prepare the wall, deal point sticks, and draw the starting hands. The next, third episode, will happen...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/10/03/mahjong-basics-02/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the third episode is live on YouTube!",
+        "excerpt":"The third episode of our series about basics of rīchi mahjong is on the YouTube channel of Kraków Chombo Club.. We explain the basic aspect of the flow of the game, namely the process of building our hand by using tiles from the wall. We also show how to declare...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/05/mahjong-basics-03/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the fourth episode is live on YouTube!",
+        "excerpt":"The fourth episode of our series about basics of rīchi mahjong is on the YouTube channel of Kraków Chombo Club. We describe the art of melding tiles discarded by other players into our hand, as well as about the rules about quad groups.   The next episodes soon!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/29/mahjong-basics-04/",
+        "teaser": null
+      },{
+        "title": "Equipment: review of the white tile set from e-go.pl",
+        "excerpt":"Hey! This is Michał Herda from the Krakow Chombo Club. Today I’m happy to show you a review of the riichi mahjong set from the online e-go.pl shop. The mahjong set cost me 269 PLN and contains tiles and accessories for playing Japanese riichi mahjong. During the review, I’ll make...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/06/15/review-e-go-white-tiles/",
+        "teaser": null
+      }]
