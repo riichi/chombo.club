31e0c63edc933a5e1b4683c5e5adef109d6ce0e8
Iipin <Iipin@users.noreply.github.com>
2022-02-18 19:02:01 +0000

    jekyll build from Action 3168eede6a3719deb02b1acc0b6f98882a9b638b

---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,67 @@
+var store = [{
+        "title": "Merry Christmas!",
+        "excerpt":"Kraków Chombo Club wishes you a Merry Christmas and Happy New Year! May the 2020 bring you many Kokushi Musous, Daisangens and Suuankous!  ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2019/12/24/merry-christmas/",
+        "teaser": null
+      },{
+        "title": "Michał Herda has become a USMA referee",
+        "excerpt":"We are happy to announce that our colleague Michał Herda has become an Upper Silesian Mahjong Association-certified referee. He is now eligible for refereeing USMA ranked games as well as non-MERS (USMA) tournaments. Congratulations!          ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/01/20/new-referee-michal-herda/",
+        "teaser": null
+      },{
+        "title": "Krakowski Klub Chombo on Mahjong Retro Taikai",
+        "excerpt":"Kraków Chombo Club participated in Retro Mahjong Taikai, proudly represented by four of our members - unfortunately, with no prizes this time :( The tournament was a great occasion to meet Mahjong freaks from other parts of Poland as well as try out playing with bamboo tiles. Plus Mateusz Maćkowski...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/02/23/mahjong-retro-taikai/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the first episode is live on YouTube!",
+        "excerpt":"YouTube lacks high-quality tutorials about Mahjong for beginners, especially in Polish. Newcomers that want to play this chinese game are therefore forced to watch videos in English or read text guides in Polish that are often not very great. A person who is completely new to the game that does...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/09/05/mahjong-basics-01/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the second episode is live on YouTube!",
+        "excerpt":"The second episode of our series describing the rīchi mahjong basics landed on the YouTube channel of Kraków Chombo Club.. We show how to properly prepare the game, that is: seat the players, prepare the wall, deal point sticks, and draw the starting hands. The next, third episode, will happen...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/10/03/mahjong-basics-02/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the third episode is live on YouTube!",
+        "excerpt":"The third episode of our series about basics of rīchi mahjong is on the YouTube channel of Kraków Chombo Club.. We explain the basic aspect of the flow of the game, namely the process of building our hand by using tiles from the wall. We also show how to declare...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/05/mahjong-basics-03/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the fourth episode is live on YouTube!",
+        "excerpt":"The fourth episode of our series about basics of rīchi mahjong is on the YouTube channel of Kraków Chombo Club. We describe the art of melding tiles discarded by other players into our hand, as well as about the rules about quad groups.   The next episodes soon!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/29/mahjong-basics-04/",
+        "teaser": null
+      },{
+        "title": "Equipment: review of the white tile set from e-go.pl",
+        "excerpt":"Hey! This is Michał Herda from the Krakow Chombo Club. Today I’m happy to show you a review of the riichi mahjong set from the online e-go.pl shop. The mahjong set cost me 269 PLN and contains tiles and accessories for playing Japanese riichi mahjong. During the review, I’ll make...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/06/15/review-e-go-white-tiles/",
+        "teaser": null
+      },{
+        "title": "Kraków Chombo Club Association",
+        "excerpt":"We’ve made it — on July 29, 2021 the Krakow Chombo Club has officially become an association. It’s the pinnacle of a nearly two-year process — one year of playing together and planning the future, and another which we devoted to the formal process and developing the idea of the...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/08/09/kcc-association/",
+        "teaser": null
+      },{
+        "title": "Kraków Riichi Open 2021",
+        "excerpt":"Just as we said back when we finished work on creating our association, Krakow Chombo Club is proud to announce the first open Riichi Mahjong tournament in Krakow! The tournament will be played with EMA rules on 20 November 2021 in Boardowa Board Game Cafe. You can find all the...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2021/09/29/krakow-riichi-open/",
+        "teaser": null
+      },{
+        "title": "Instant Ramen Taikai 2022",
+        "excerpt":"It’s time for the first chicken-flavored riichi tournament in Kraków! The tournament will be held on 3rd April 2022 in Boardowa Cafe and will use modified EMA rules. Additionally, the tournament will be included in the Upper-Silesian Mahjong Association ranking. Below you will find all the tournament details and the...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/02/15/instant-ramen-taikai/",
+        "teaser": null
+      }]
