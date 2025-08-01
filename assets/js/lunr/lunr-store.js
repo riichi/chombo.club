fb3384d3cc86f63d26b95997f642a4074a38c7f3
Mateusz Maćkowski <m4tx@m4tx.pl>
2025-08-01 11:26:39 +0200

    deploy: 7100974

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,115 @@
+var store = [{
+        "title": "Wesołych Świąt!",
+        "excerpt":"Krakowski Klub Chombo życzy wszystkim Wesołych Świąt i Szczęśliwego Nowego Roku! Niech 2020 przyniesie Wam wiele Kokushi Musou, Daisangenów i Suuankou!  ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2019/12/24/merry-christmas/",
+        "teaser": null
+      },{
+        "title": "Michał Herda sędzią USMA",
+        "excerpt":"Z radością informujemy, że nasz klubowy kolega Michał Herda został certyfikowanym sędzią Górnośląskiego Związku Madżonga. Będzie on mógł od teraz sędziować gry rankingowe USMA oraz nie-MERS-owe (USMA) turnieje. Gratulacje!           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/01/20/new-referee-michal-herda/",
+        "teaser": null
+      },{
+        "title": "Krakowski Klub Chombo na Mahjong Retro Taikai",
+        "excerpt":"Czteroosobowa reprezentacja Krakowskiego Klubu Chombo wybrała się na Retro Mahjong Taikai i przywiozła trofea w postaci zupełnie niczego! Turniej był wspaniały, graliśmy na bambusowych kamieniach, a Mateusz Maćkowski dorobił się w warszawskim Auchanie nowej emotikony.           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/02/23/mahjong-retro-taikai/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — pierwszy odcinek już na YouTube!",
+        "excerpt":"Na polskim YouTube brakuje wartościowych materiałów o mahjongu dla początkujących. Początkujący chcący zagrać w tę chińską grę towarzyską skazani są na wideo w języku angielskim lub na materiały w formie tekstowej po polsku o często wątpliwej jakości. Osoba kompletnie nieobeznana w temacie oraz bez kogoś, kto pomógłby jej nauczyć się...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/09/05/mahjong-basics-01/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — drugi odcinek już na YouTube!",
+        "excerpt":"Drugi odcinek naszej serii filmów o podstawach rīchi mahjonga znalazł się na kanale Krakowskiego Klubu Chombo na YouTube.. Pokazujemy na nim, w jaki sposób poprawnie przygotować grę, to znaczy: usadzić graczy, przygotować mur, rozdać punkty, oraz dobrać ręce startowe.   Kolejny, trzeci odcinek, już niebawem!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/10/03/mahjong-basics-02/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — trzeci odcinek już na YouTube!",
+        "excerpt":"Trzeci odcinek naszej serii filmów o podstawach rīchi mahjonga znalazł się na kanale Krakowskiego Klubu Chombo na YouTube.. Przechodzimy w nim wreszcie do podstaw przebiegu gry, czyli procesu budowania ręki przy użyciu kamieni z muru oraz deklarowania wygranej.   Czwarty odcinek już wkrótce!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/05/mahjong-basics-03/",
+        "teaser": null
+      },{
+        "title": "Podstawy Rīchi Mahjonga — czwarty odcinek już na YouTube!",
+        "excerpt":"Czwarty odcinek naszej serii filmów o podstawach rīchi mahjonga znalazł się na kanale Krakowskiego Klubu Chombo na YouTube. Opowiadamy w nich o sztuce złodziejstwa, czyli meldowania kamieni wyrzuconych przez przeciwnika, oraz o zasadach dotyczących czwórek.   Kolejne odcinki już wkrótce!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/29/mahjong-basics-04/",
+        "teaser": null
+      },{
+        "title": "Ekwipunek: recenzja zestawu białego z e-go.pl",
+        "excerpt":"Cześć! Tutaj Michał Herda z Krakowskiego Klubu Chombo. Dzisiaj prezentuję recenzję zestawu do mahjonga japońskiego z internetowego sklepu e-go.pl. Zestaw w momencie zakupu kosztował 269 PLN (stan na marzec 2024: 449 PLN) i zawiera kamienie oraz akcesoria do gry w mahjonga japońskiego. W trakcie recenzji zestaw będę tu i ówdzie...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/06/15/review-e-go-white-tiles/",
+        "teaser": null
+      },{
+        "title": "Stowarzyszenie Krakowski Klub Chombo",
+        "excerpt":"Stało się — z dniem 29 lipca 2021 roku Krakowski Klub Chombo stał się oficjalnie stowarzyszeniem. Jest to zwieńczenie niemal dwuletniego procesu — jednego roku wspólnej gry i planów na przyszłość, oraz kolejnego, który poświęciliśmy na proces formalny i rozwój idei stowarzyszenia. Mamy nadzieję, że oficjalna forma stowarzyszenia pozwoli nam...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/08/09/kcc-association/",
+        "teaser": null
+      },{
+        "title": "Kraków Riichi Open 2021",
+        "excerpt":"Zgodnie z zapowiedzią przy okazji ogłoszenia o utworzeniu stowarzyszenia, Krakowski Klub Chombo ma przyjemność ogłosić pierwszy otwarty krakowski turniej Riichi Mahjonga! Turniej będzie rozgrywany na zasadach EMA i odbędzie się 20 listopada 2021 roku w kawiarni Boardowa. Dodatkowo, turniej będzie uwzględniony w rankingu Górnośląskiego Związku Madżonga. Poniżej znajdziecie wszystkie szczegóły...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2021/09/29/krakow-riichi-open/",
+        "teaser": null
+      },{
+        "title": "Instant Ramen Taikai 2022",
+        "excerpt":"Czas na pierwszy krakowski turniej riichi o smaku kurczaka! Turniej odbędzie się 3 kwietnia 2022 roku w kawiarni Boardowa i będzie rozgrywany na zmodyfikowanych zasadach EMA. Dodatkowo, turniej będzie uwzględniony w rankingu Górnośląskiego Związku Madżonga. Poniżej znajdziecie wszystkie szczegóły dotyczące turnieju oraz link do formularza rejestracyjnego. Zgłoszenia przyjmujemy do 20...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/02/15/instant-ramen-taikai/",
+        "teaser": null
+      },{
+        "title": "Kraków Saki Cards Taikai 2022",
+        "excerpt":"Zapraszamy Was na pierwszy w Polsce turniej mahjonga z Saki Kartami! Odbędzie się już 14 sierpnia 2022 w kawiarni Boardowa. O turnieju Data: 14 sierpnia 2022 (niedziela) Miejsce: Boardowa, ul. Topolowa 52, 31-506 Kraków Zasady: modyfikowane EMA 2016; uma +15/+5/-5/-15; bez oki Hanchany: 4 Sędziowie: Michał Herda (nie będzie grał),...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/06/27/cracow-saki-cards-taikai/",
+        "teaser": null
+      },{
+        "title": "Kraków MERS Open 2022",
+        "excerpt":"Zapraszamy Was na pierwszy turniej MERS w Krakowie! O turnieju Data: 8-9 października 2022 (sobota-niedziela) Miejsce: Wydział Matematyki i Informatyki UJ, prof. Stanisława Łojasiewicza 6, 30-348 Kraków Zasady: EMA Riichi 2016 Hanchany: 7 Opłata: 100PLN lub 22EUR, płatne na miejscu lub przelewem (tylko PLN): Nr konta: 13 1140 2004 0000...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/07/09/krakow-mers-open/",
+        "teaser": null
+      },{
+        "title": "Mateusz Maćkowski sędzią EMA",
+        "excerpt":"W ten weekend (20-21.05.2023) nasza reprezentacja zameldowała się w Kopenhadze na Open Danish Championship. Przed weekendowymi zmaganiami odbyło się seminarium sędziowskie EMA, podczas którego Mateusz Maćkowski uzyskał tytuł certyfikowanego sędziego!   Serdecznie gratulujemy!           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2023/05/20/new-referee-mateusz-mackowski/",
+        "teaser": null
+      },{
+        "title": "Hatsumi Taikai",
+        "excerpt":"Zapraszamy Was na drugi turniej MERS w Krakowie! O turnieju Data: 17-18 lutego 2024 (sobota-niedziela) Miejsce: Wydział Matematyki i Informatyki UJ, prof. Stanisława Łojasiewicza 6, 30-348 Kraków Zasady: EMA Riichi 2016 Hanchany: 8 Wpisowe: 150 PLN (35 EUR), płatne przelewem: Płatność do 7 lutego 2024 Nr konta: 13 1140 2004...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2023/10/06/hatsumi-taikai/",
+        "teaser": null
+      },{
+        "title": "Szymon Nowicz sędzią EMA",
+        "excerpt":"Przed turniejem w Poznaniu, w piątek 10.11.2023, odbyło się seminarium EMA, podczas którego Szymon Nowicz zdobył uprawnienia do sędziowania zawodów organizowanych w ramach Europejskiego Związku Mahjonga (warto wspomnieć, że Szymon zdobył najwyższy wynik na teście!)   Serdecznie gratulujemy!           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2023/11/15/new-referee-szymon-nowicz/",
+        "teaser": null
+      },{
+        "title": "Mamy siedzibę!",
+        "excerpt":"Jak być może już wiecie, udało nam się pozyskać siedzibę. Teraz nadeszła pora na ogłoszenie regularnych spotkań, które będą się w niej odbywać! Organizować będziemy dwa typy spotkań — luźne granie oraz gry rankingowe zaliczane do rankingu USMA, prowadzonego przez naszych śląskich przyjaciół. Szczegóły: Adres siedziby: ul. Studencka 6/8, 31-116...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2024/07/27/headquarters/",
+        "teaser": null
+      },{
+        "title": "Kupowanie zestawu do mahjonga — jak wybrać ten najlepszy?",
+        "excerpt":"Ostatnimi czasy najczęściej zadawanymi pytaniami - zarówno na naszym serwerze na Discordzie, w wiadomościach na Facebooku oraz mailowo – są te poruszające kwestie, gdzie można kupić zestaw do mahjonga, ile kosztuje takowy oraz jak odróżnić zestaw do mahjonga japońskiego od innych. Wspólnie postanowiliśmy stworzyć poradnik, który powinien w pewnym stopniu...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2024/12/13/how-to-buy-mahjong-set/",
+        "teaser": null
+      },{
+        "title": "Kraków Saki Cards Taikai 2025",
+        "excerpt":"Zapraszamy Was na drugi w Polsce turniej mahjonga z Saki Kartami! O turnieju Data: 26 lipca 2025 (sobota) Miejsce: Siedziba Krakowskiego Klubu Chombo Zasady: modified EMA Riichi 2016 + saki cards 1.3 Hanchany: 4 Wpisowe: 55 PLN (13 EUR), płatne przelewem: Płatność do 25 lipca 2025 Nr konta: 13 1140...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2025/05/17/cracow-saki-cards-taikai-2025/",
+        "teaser": null
+      }]
---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,109 @@
+var store = [{
+        "title": "Merry Christmas!",
+        "excerpt":"Kraków Chombo Club wishes you a Merry Christmas and Happy New Year! May the 2020 bring you many Kokushi Musous, Daisangens and Suuankous!  ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2019/12/24/merry-christmas/",
+        "teaser": null
+      },{
+        "title": "Michał Herda has become a USMA referee",
+        "excerpt":"We are happy to announce that our colleague Michał Herda has become an Upper Silesian Mahjong Association-certified referee. He is now eligible for refereeing USMA ranked games as well as non-MERS (USMA) tournaments. Congratulations!           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/01/20/new-referee-michal-herda/",
+        "teaser": null
+      },{
+        "title": "Krakowski Klub Chombo on Mahjong Retro Taikai",
+        "excerpt":"Kraków Chombo Club participated in Retro Mahjong Taikai, proudly represented by four of our members - unfortunately, with no prizes this time :( The tournament was a great occasion to meet Mahjong freaks from other parts of Poland as well as try out playing with bamboo tiles. Plus Mateusz Maćkowski...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/02/23/mahjong-retro-taikai/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the first episode is live on YouTube!",
+        "excerpt":"YouTube lacks high-quality tutorials about Mahjong for beginners, especially in Polish. Newcomers that want to play this chinese game are therefore forced to watch videos in English or read text guides in Polish that are often not very great. A person who is completely new to the game that does...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/09/05/mahjong-basics-01/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the second episode is live on YouTube!",
+        "excerpt":"The second episode of our series describing the rīchi mahjong basics landed on the YouTube channel of Kraków Chombo Club.. We show how to properly prepare the game, that is: seat the players, prepare the wall, deal point sticks, and draw the starting hands. The next, third episode, will happen...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/10/03/mahjong-basics-02/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the third episode is live on YouTube!",
+        "excerpt":"The third episode of our series about basics of rīchi mahjong is on the YouTube channel of Kraków Chombo Club.. We explain the basic aspect of the flow of the game, namely the process of building our hand by using tiles from the wall. We also show how to declare...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/05/mahjong-basics-03/",
+        "teaser": null
+      },{
+        "title": "Rīchi Mahjong Basics — the fourth episode is live on YouTube!",
+        "excerpt":"The fourth episode of our series about basics of rīchi mahjong is on the YouTube channel of Kraków Chombo Club. We describe the art of melding tiles discarded by other players into our hand, as well as about the rules about quad groups.   The next episodes soon!    ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2020/11/29/mahjong-basics-04/",
+        "teaser": null
+      },{
+        "title": "Equipment: review of the white tile set from e-go.pl",
+        "excerpt":"Hey! This is Michał Herda from the Krakow Chombo Club. Today I’m happy to show you a review of the riichi mahjong set from the online e-go.pl shop. The mahjong set cost me 269 PLN (March 2024 update: 449 PLN) and contains tiles and accessories for playing Japanese riichi mahjong....","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/06/15/review-e-go-white-tiles/",
+        "teaser": null
+      },{
+        "title": "Kraków Chombo Club Association",
+        "excerpt":"We’ve made it — on July 29, 2021 the Krakow Chombo Club has officially become an association. It’s the pinnacle of a nearly two-year process — one year of playing together and planning the future, and another which we devoted to the formal process and developing the idea of the...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2021/08/09/kcc-association/",
+        "teaser": null
+      },{
+        "title": "Kraków Riichi Open 2021",
+        "excerpt":"Just as we said back when we finished work on creating our association, Krakow Chombo Club is proud to announce the first open Riichi Mahjong tournament in Krakow! The tournament will be played with EMA rules on 20 November 2021 in Boardowa Board Game Cafe. You can find all the...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2021/09/29/krakow-riichi-open/",
+        "teaser": null
+      },{
+        "title": "Instant Ramen Taikai 2022",
+        "excerpt":"It’s time for the first chicken-flavored riichi tournament in Kraków! The tournament will be held on 3rd April 2022 in Boardowa Cafe and will use modified EMA rules. Additionally, the tournament will be included in the Upper-Silesian Mahjong Association ranking. Below you will find all the tournament details and the...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/02/15/instant-ramen-taikai/",
+        "teaser": null
+      },{
+        "title": "Kraków Saki Cards Taikai 2022",
+        "excerpt":"We invite you to the first ever Polish tournament with Saki Cards! It will take place at 14th August 2022 in the Boardowa cafe. About Date: 14th August 2022 (Sunday) Place: Boardowa Cafe, Topolowa 52, 31-506 Kraków Rules: modified EMA 2016; uma +15/+5/-5/-15; no oka Hanchans: 4 Judges: Michał Herda...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/06/27/cracow-saki-cards-taikai/",
+        "teaser": null
+      },{
+        "title": "Kraków MERS Open 2022",
+        "excerpt":"We invite you to the first MERS tournament in Kraków! About Date: 8th-9th October 2022 (Sat-Sun) Place: Jagiellonian University, Faculty of Mathematics and Information Technologies, Stanisława Łojasiewicza 6, 30-348 Kraków Ruleset: EMA Riichi 2016 Hanchans: 7 Entry fee: 100PLN or 22EUR, paid at the venue or by bank transfer (PLN...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2022/07/09/krakow-mers-open/",
+        "teaser": null
+      },{
+        "title": "Mateusz Maćkowski has become an EMA referee",
+        "excerpt":"This weekend (20-21 May 2023), our national team went to Copenhagen for the Open Danish Championship. Before the weekend’s battles, an EMA refereeing seminar was held, during which Mateusz Maćkowski was awarded the title of certified referee!   Congratulations!           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2023/05/20/new-referee-mateusz-mackowski/",
+        "teaser": null
+      },{
+        "title": "Hatsumi Taikai",
+        "excerpt":"We invite you to the second MERS tournament in Kraków! About Date: 17th-18th February 2024 (Sat-Sun) Place: Jagiellonian University, Faculty of Mathematics and Information Technologies, Stanisława Łojasiewicza 6, 30-348 Kraków, Poland Ruleset: EMA Riichi 2016 Hanchans: 8 Entry fee: 150 PLN (35 EUR), paid by bank transfer (make sure you...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2023/10/06/hatsumi-taikai/",
+        "teaser": null
+      },{
+        "title": "Szymon Nowicz has become an EMA referee",
+        "excerpt":"Just before the tournament in Poznań, on Friday 10 Nov 2023, an EMA seminar was held, during which Szymon Nowicz was qualified to judge competitions organised within the European Mahjong Association (it’s worth mentioning that Szymon obtained the highest score on the test!).   Congratulations!           ","categories": ["other"],
+        "tags": [],
+        "url": "/other/2023/11/15/new-referee-szymon-nowicz/",
+        "teaser": null
+      },{
+        "title": "We've got a headquarters!",
+        "excerpt":"As some of you might already know, we’ve managed to secure our own place to play mahjong. Now it’s time to share some details regarding regular meetings that’ll be taking place there! We’ll be organizing two types of meetings — casual games and ranked games that’ll count towards the USMA...","categories": ["other"],
+        "tags": [],
+        "url": "/other/2024/07/27/headquarters/",
+        "teaser": null
+      },{
+        "title": "Kraków Saki Cards Taikai 2025",
+        "excerpt":"We invite you to the second-ever Polish tournament using Saki Cards! About Date: 26th July 2025 (Saturday) Place: Headquarters of Kraków Chombo Club Rules: modified EMA Riichi 2016 + Saki Cards 1.3 Hanchans: 4 Fee: 55 PLN (13 EUR), paid by bank transfer (make sure you cover all the bank...","categories": ["tournaments"],
+        "tags": [],
+        "url": "/tournaments/2025/05/17/cracow-saki-cards-taikai-2025/",
+        "teaser": null
+      }]
