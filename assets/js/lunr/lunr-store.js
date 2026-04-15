b0e290b408f49005d290006f0259f09c430c0176
5zlotych <5zlotych@users.noreply.github.com>
2026-04-15 11:26:25 +0000

    deploy: 5868b5a77d379c1c996303b0e66a75310082eecd

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -126,7 +126,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Mahjong Parlors In Japan A Blog Post",
-        "excerpt":"--- title: \"Grałem w 4* free-rate parlorach w Japonii, moje przemyślenia\" date: 2026-04-15 12:00:00 +0200 categories: blog lang: pl --- Podczas mojego dwutygodniowego pobytu w Japonii miałem okazję zagrać w kilku parlorach mahjonga w Tokio i Osace. Poniżej przedstawiam moje przemyślenia i doświadczenia z wizyt w tych miejscach. ## Bellbird...","categories": [],
+        "excerpt":"title: “Grałem w 4* free-rate parlorach w Japonii, moje przemyślenia” date: 2026-04-15 12:00:00 +0200 categories: blog lang: pl — Podczas mojego dwutygodniowego pobytu w Japonii miałem okazję zagrać w kilku parlorach mahjonga w Tokio i Osace. Poniżej przedstawiam moje przemyślenia i doświadczenia z wizyt w tych miejscach. Bellbird Shinbashi Lokalizacja:...","categories": [],
         "tags": [],
         "url": "/2026/04/15/Mahjong-parlors-in-japan-a-blog-post/",
         "teaser": null
